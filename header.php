4a470f3bc267089410a735b171dde94585825a90
ClemensLosbichler <60425687+ClemensLosbichler@users.noreply.github.com>
2020-09-13 19:00:25 +0200

    forbiddne

--- header.php
+++ header.php
@@ -6,12 +6,12 @@
     <div class="collapse navbar-collapse">
     <ul class="navbar-nav">
         <li class="nav-item"><a class="nav-link" href="/"><i class="fas fa-home"></i></a></li>
-        <li class="nav-item"><a class="nav-link" href="upload.html"><i class="fas fa-upload"></i></a></li>
+        <li class="nav-item"><a class="nav-link" href="/upload"><i class="fas fa-upload"></i></a></li>
     </ul>
     </div>
     <div class="navbar-collapse collapse">
     <ul class="navbar-nav ml-auto">
-        <li class="nav-item"><a class="nav-link" href="settings.html"><i class="fas fa-cog"></i></a></li>
+        <li class="nav-item"><a class="nav-link" href="/settings"><i class="fas fa-cog"></i></a></li>
     </ul>
     </div>
 </nav>  
