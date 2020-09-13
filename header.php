f156c28765b599d2b76257c525113895be6aed3b
ClemensLosbichler <60425687+ClemensLosbichler@users.noreply.github.com>
2020-09-13 18:32:01 +0200

    php

---
+++ header.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href=""></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse">
+    <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link" href="/"><i class="fas fa-home"></i></a></li>
+        <li class="nav-item"><a class="nav-link" href="upload.html"><i class="fas fa-upload"></i></a></li>
+    </ul>
+    </div>
+    <div class="navbar-collapse collapse">
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item"><a class="nav-link" href="settings.html"><i class="fas fa-cog"></i></a></li>
+    </ul>
+    </div>
+</nav>  
